fda472434387a74498017b4fec889721ad1ff06e
Arnab Kundu <arnabk@mindfiresolutions.com>
2023-11-16 15:15:44 +0530

    add heading to landing page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Canvas from './components/Canvas';
 function App() {
   return (
     <div className="App">
+      <h1 style={{ color: "red" }}>This is a HTML Canvas demo project</h1>
       <Canvas width={window.innerWidth} height={window.innerHeight} canvasCtx="2d" />
     </div>
   );
